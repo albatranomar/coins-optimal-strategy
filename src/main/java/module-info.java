d4aa7fa0a4e4800ecb13fa8a5e80b7459a6284d2
albatranomar <albatranomar943@gmail.com>
2024-11-02 15:00:26 +0200

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module algo.algorithmscomp336 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens algo.algorithmscomp336 to javafx.fxml;
+    exports algo.algorithmscomp336;
+}

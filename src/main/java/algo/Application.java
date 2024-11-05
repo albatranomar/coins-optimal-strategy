07a8eabe68b5a12a21044c0d6e70fbac35c4326d
albatranomar <albatranomar943@gmail.com>
2024-11-05 22:54:16 +0200

    feat(solution): introduce Solution class
    Added a new `Solution` class and moved all related methods into it to better encapsulate problem-specific functionality.

--- src/main/java/algo/Application.java
+++ src/main/java/algo/Application.java
@@ -1,5 +1,6 @@
 package algo;
 
+import algo.solution.Solution;
 import algo.util.FXMLUtil;
 import algo.util.View;
 import javafx.scene.Scene;
@@ -8,9 +9,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// This is the main entry class the first scene is loaded and displayed here
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load the first scene of the application
         BorderPane borderPane = FXMLUtil.load(View.LANDING);
         Scene scene = new Scene(borderPane, 1024, 768);
         stage.setTitle("Omar Albatran - 1221344");
@@ -19,6 +22,10 @@ public class Application extends javafx.application.Application {
     }
 
     public static void main(String[] args) {
+        var c = new int[]{8, 15, 3, 7, 6, 5};
+        var t = Solution.solve(c);
+        var r = Solution.coinsTrace(c, t);
+
         launch();
     }
 }
---
+++ src/main/java/algo/solution/Solution.java
@@ -0,0 +1,112 @@
+package algo.solution;
+
+// This solution class contains all the algorithms related to solving the optimal strategy game
+public class Solution {
+
+    // Solve the problem given the input coins and returning the generated table
+    public static int[][] solve(int[] coins) {
+        int n = coins.length;
+
+        if (n % 2 != 0) {
+            System.out.println("The number of coins should be an even number!");
+            return null;
+        }
+
+        int[][] table = new int[n][n];
+
+        // Iterate the table diagonally
+        for (int col = 0; col < n; col++) {
+            for (int i = 0, j = col; i < n && j < n; i++, j++) {
+                // the main diagonal, the initial values (a single coin)
+                if (i == j) {
+                    table[i][j] = coins[i];
+                    continue;
+                }
+
+                // handles the second diagonal to avoid OutOfBounds errors
+                if (j - i == 1) {
+                    // take the maximum between the left most and the right most
+                    table[i][j] = Math.max(table[i][j - 1], table[i + 1][j]);
+                    continue;
+                }
+
+                // if chosen left most coin what is the gain
+                int takenLeft = coins[i];
+                // since the other player is playing optimally after chosen the left most coin
+                // he will leave me the minimum he can
+                takenLeft += Math.min(table[i + 1][j - 1], table[i + 2][j]);
+
+                // if chosen right most coin what is the gain
+                int takenRight = coins[j];
+                // since the other player is playing optimally after chosen the right most coin
+                // he will leave me the minimum he can
+                takenRight += Math.min(table[i][j - 2], table[i + 1][j - 1]);
+
+                // take the maximum of either choice
+                table[i][j] = Math.max(takenLeft, takenRight);
+            }
+        }
+
+        return table;
+    }
+
+    // Use the generated table to trace back the order in which the coins where taken by the first player
+    // [int[] coins] being the input coins
+    public static int[] coinsTrace(int[] coins, int[][] table) {
+        int n = coins.length;
+
+        if (n % 2 != 0) {
+            System.out.println("The number of coins should be an even number!");
+            return null;
+        }
+
+        int[] result = new int[n / 2];
+
+        coinsTraceHelper(0, n - 1, coins, table, result, 0);
+
+        return result;
+    }
+
+    private static void coinsTraceHelper(int i, int j, int[] coins, int[][] table, int[] res, int next) {
+        // Stop condition: when reaching OutOfBounds
+        if (i > j) return;
+
+        // Stop condition: reached the end of the game
+        if (i == j && next < res.length) {
+            res[next] = coins[i];
+            return;
+        }
+
+        // if chosen left most coin what is the gain
+        int takenLeft = coins[i];
+        // since the other player is playing optimally after chosen the left most coin
+        // he will leave me the minimum he can
+        takenLeft += Math.min(table[i + 1][j - 1], table[i + 2][j]);
+
+        // if chosen right most coin what is the gain
+        int takenRight = coins[j];
+        // since the other player is playing optimally after chosen the right most coin
+        // he will leave me the minimum he can
+        takenRight += Math.min(table[i][j - 2], table[i + 1][j - 1]);
+
+        if (takenRight > takenLeft) {
+            res[next] = coins[j];
+            next++;
+
+            if (table[i][j - 2] < table[i + 1][j - 1]) {
+                coinsTraceHelper(i, j - 2, coins, table, res, next);
+            } else {
+                coinsTraceHelper(i + 1, j - 1, coins, table, res, next);
+            }
+        } else {
+            res[next] = coins[i];
+            next++;
+
+            if (table[i + 1][j - 1] < table[i + 2][j]) {
+                coinsTraceHelper(i + 1, j - 1, coins, table, res, next);
+            } else {
+                coinsTraceHelper(i + 2, j, coins, table, res, next);
+            }
+        }
+    }
+}

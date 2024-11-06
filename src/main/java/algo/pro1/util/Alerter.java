2ee46c3fe7a19f7a3b656ff26e0f404f71e9ec77
albatranomar <albatranomar943@gmail.com>
2024-11-06 22:01:44 +0200

    feat(alert): introduce Alerter to simplify alert system

---
+++ src/main/java/algo/pro1/util/Alerter.java
@@ -0,0 +1,45 @@
+package algo.pro1.util;
+
+import javafx.scene.control.Alert;
+
+public class Alerter {
+    private static Alert create(Alert.AlertType type, String title, String content) {
+        Alert alert = new Alert(type);
+        if (title != null) alert.setHeaderText(title);
+        if (content != null) alert.setContentText(content);
+
+        return alert;
+    }
+
+    public static Alert info(String content) {
+        return info(null, content);
+    }
+
+    public static Alert info(String title, String content) {
+        return create(Alert.AlertType.INFORMATION, title, content);
+    }
+
+    public static Alert warn(String content) {
+        return warn(null, content);
+    }
+
+    public static Alert warn(String title, String content) {
+        return create(Alert.AlertType.INFORMATION, title, content);
+    }
+
+    public static Alert error(String content) {
+        return error(null, content);
+    }
+
+    public static Alert error(String title, String content) {
+        return create(Alert.AlertType.ERROR, title, content);
+    }
+
+    public static Alert confirm(String content) {
+        return confirm(null, content);
+    }
+
+    public static Alert confirm(String title, String content) {
+        return create(Alert.AlertType.CONFIRMATION, title, content);
+    }
+}

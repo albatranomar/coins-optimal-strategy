6834e3bda3b75be8763618d64da003f5644c4f59
albatranomar <albatranomar943@gmail.com>
2024-11-08 00:20:34 +0200

    chore(SolutionTable): unused import

--- src/main/java/algo/pro1/controllers/SolutionTableController.java
+++ src/main/java/algo/pro1/controllers/SolutionTableController.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.Arrays;
 
